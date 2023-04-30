e894c8b1943f8572d99af81074318113be587e75
Acer Zhou <acerzhou.work@gmail.com>
2023-04-30 15:15:35 +1000

    Update git ignore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
+import ChatBot from "./components/ChatBot";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Container = styled.div``;
 
 function App() {
+  const [isChatBotShow, setIsChatBotShow] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      this is home page
+      <div>
+        <button onClick={() => setIsChatBotShow(!isChatBotShow)}>
+          chat bot{" "}
+        </button>
+      </div>
+      {isChatBotShow && <ChatBot />}
+    </Container>
   );
 }
 
